d3e5237bca1f7b133630c16f4dfd33d435e172b8
Adam Lazarus <adamlaz@gmail.com>
2023-11-16 20:40:31 -0500

    more logging

--- api/webhook.js
+++ api/webhook.js
@@ -1,10 +1,13 @@
 const Box = require('box-node-sdk');
 
 export default async function webhook(request, response) {
+  console.log('Webhook triggered'); // Test log
+
   let isValid = Box.validateWebhookMessage(request.body, request.headers, process.env.primaryKey, process.env.secondaryKey);
 
   if (isValid) {
-    // Configure SDK with environment variables
+    console.log('Webhook is valid'); // Log validation success
+
     const config = {
       boxAppSettings: {
         clientID: process.env.clientID,
@@ -22,26 +25,31 @@ export default async function webhook(request, response) {
     const client = sdk.getAppAuthClient('enterprise', process.env.enterpriseID);
 
     try {
-      const fileId = request.body.source.id;
-      console.log('File ID:', fileId); // Log the file ID
+      console.log('Request Body:', request.body); // Log the entire request body
 
-      // Apply metadata template to the file
-      const metadataTemplate = 'authorizingDocument';
-      const metadataBody = {
-        test1: 'asdf',
-        test2: '1234'
-      };
+      try {
+        const fileId = request.body.source.id;
+        console.log('File ID:', fileId); // Log the file ID
 
-      const metadataResponse = await client.files.addMetadata(fileId, client.metadata.scopes.ENTERPRISE, metadataTemplate, metadataBody);
-      console.log('Metadata Response:', metadataResponse); // Log the response
+        // Apply metadata template to the file
+        const metadataTemplate = 'authorizingDocument';
+        const metadataBody = {
+          test1: 'asdf',
+          test2: '1234'
+        };
 
-      response.status(200).json({ info: 'success' });
-    } catch (error) {
-      console.error('Error applying metadata:', error);
-      response.status(200).json({ info: 'Error', error: error.toString() });
+        const metadataResponse = await client.files.addMetadata(fileId, client.metadata.scopes.ENTERPRISE, metadataTemplate, metadataBody);
+        console.log('Metadata Response:', metadataResponse); // Log the response
+
+        response.status(200).json({ info: 'success' });
+
+      } catch (error) {
+        console.error('Error applying metadata:', error);
+        response.status(200).json({ info: 'Error', error: error.toString() });
+      }
+
+    } else {
+      console.error('Invalid webhook message');
+      response.status(200).json({ info: 'Invalid webhook message' });
     }
-
-  } else {
-    response.status(200).json({ info: 'Invalid webhook message' });
   }
-}
