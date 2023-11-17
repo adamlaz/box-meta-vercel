8faef9187d7497d754a847c8906d35120d2a7b89
Adam Lazarus <adamlaz@gmail.com>
2023-11-16 19:51:10 -0500

    refactor: to metadata template

--- api/webhook.js
+++ api/webhook.js
@@ -1,37 +1,40 @@
 const Box = require('box-node-sdk');
 
 export default async function webhook(request, response) {
+  // Validate that Box is the service calling the webhook.
+  let isValid = Box.validateWebhookMessage(request.body, request.headers, process.env.primaryKey, process.env.secondaryKey);
 
-    //This line will validate that box is the service calling the webhook. 
-    let isValid = Box.validateWebhookMessage(request.body, request.headers, process.env.primaryKey, process.env.secondaryKey);
+  if (isValid) {
+    const sdkConfig = {
+      boxAppSettings: {
+        clientID: process.env.clientID,
+        clientSecret: process.env.clientSecret
+      },
+      enterpriseID: process.env.enterpriseID
+    };
+    const sdk = Box.getPreconfiguredInstance(sdkConfig);
+    const client = sdk.getAppAuthClient('enterprise', process.env.enterpriseID);
 
-    if (isValid) {
-      const sdkConfig = {
-          boxAppSettings: {
-              clientID: process.env.clientID,
-              clientSecret: process.env.clientSecret
-          }, 
-          enterpriseID: process.env.enterpriseID 
-      }
-      const sdk = Box.getPreconfiguredInstance(sdkConfig)
+    try {
+      const fileId = request.body.source.id;
 
-      const client = sdk.getAnonymousClient(); 
-      
-      await client.fileRequests.copy(process.env.filerequestID, {
-        folder: {
-          id: request.body.source.id,
-          type: request.body.source.type
-        },
-        //You can edit this field (or remove it)
-        title: "Case Document Request"
-      })
-    //If the service fails for some reason, still return a 200. This keeps Box from calling again and again.    
-      response.status(200).json({
-        info: 'success'
-      });
-    } else {
-      response.status(200).json({
-        info: 'Error'
-      });
+      // Apply metadata template to the file
+      const metadataTemplate = 'authorizingDocument';
+      const metadataBody = {
+        test1: 'asdf',
+        test2: '1234'
+      };
+
+      await client.files.addMetadata(fileId, client.metadata.scopes.ENTERPRISE, metadataTemplate, metadataBody);
+
+      // Success response
+      response.status(200).json({ info: 'success' });
+    } catch (error) {
+      console.error('Error applying metadata:', error);
+      // Error response
+      response.status(200).json({ info: 'Error' });
     }
+  } else {
+    response.status(200).json({ info: 'Invalid webhook message' });
   }
+}
